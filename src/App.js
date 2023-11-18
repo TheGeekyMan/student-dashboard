8068abc15f8f6ff0aee848cdc808254314ade4a1
Onkar Kulkarni <64125451+TheGeekyMan@users.noreply.github.com>
2023-11-18 19:10:48 +0530

    fixed login ui

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import Layout from './Layout';
-import Routes from "./common/routes";
+import RoutesComponent from "./common/routes";
 
 function App() {
 
   return (
     <BrowserRouter>
       <Layout>
-        <Routes />
+        <RoutesComponent />
       </Layout>
     </BrowserRouter>
   );
--- src/Layout.js
+++ src/Layout.js
@@ -2,18 +2,29 @@ import React from "react";
 import SideBar from "./components/sideBar/sideBar";
 import Header from "./components/header/header";
 import styles from './layout.module.scss';
+import Login from "./components/login/login";
 
-const Layout = ({children}) => {
+const Layout = ({ children }) => {
+
+    const currentPathName = window.location.pathname;
+
     return (
-        <div className={styles.container}>
-            <div className={styles.sideBarContainer}>
-                <SideBar />
-            </div>
-            <div className={styles.rightSideContainer}>
-                <Header />
-                {children}
-            </div>
-        </div>
+        <>
+            {
+                currentPathName !== '/login' ?
+                    <div className={styles.container}>
+                        <div className={styles.sideBarContainer}>
+                            <SideBar />
+                        </div>
+                        <div className={styles.rightSideContainer}>
+                            <Header />
+                            {children}
+                        </div>
+                    </div> 
+                :
+                    <Login />
+            }
+        </>
     )
 }
 
