63501a7d3d92895ac5f6ca4af54dbfb71be96e4e
onkar <onksgk@gmail.com>
2023-11-19 16:55:47 +0530

    added schedule page

--- src/components/schedule/schedule.js
+++ src/components/schedule/schedule.js
@@ -1,8 +1,22 @@
 import React from "react";
+import { ScheduleList } from "../../constants/constants";
+import styles from './schedule.module.scss';
 
 const Schedule = () => {
     return(
-        <div>Schedule</div>
+        <div className={styles.container}>
+            <p className={styles.title}>Schedule</p>
+            <div className={styles.content}>
+                {ScheduleList.map((item,index)=>{
+                    return(
+                        <ul key={index}>
+                            <li className={styles.listTitle}>{item.title}</li>
+                            <p className={styles.listSubTitle}>{item.subTitle}</p>
+                        </ul>
+                    )
+                })}
+            </div>
+        </div>
     )
 }
 
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -9,3 +9,11 @@ export const FinanaceData = [
     { logo: TotalPaidLogo, amt: '$5,000', title: 'Total Paid' },
     { logo: OthersLogo, amt: '$300', title: 'Others' }
 ]
+
+export const ScheduleList = [
+    {title:'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', subTitle:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nec neque arcu. Quisque posuere sodales condimentum. Donec dictum risus lacinia, pharetra erat ac, fringilla tellus. Quisque laoreet nulla vel justo rhoncus tempus.'},
+    {title:'Donec fermentum sem sit amet nisi pulvinar rutrum.', subTitle:'Aliquam vitae tellus vel quam condimentum molestie eget non leo. Ut posuere ante ac nunc fermentum tristique a vel turpis. Phasellus sollicitudin risus sed lacus cursus blandit. Fusce fringilla elit sed turpis congue, vel tempus leo ultrices.'},
+    {title:'Phasellus scelerisque leo sed ultrices scelerisque.', subTitle:'Nam vitae mi vel lectus venenatis feugiat. Vivamus hendrerit orci diam, at volutpat nibh molestie et. Aliquam id neque magna. Pellentesque vulputate vulputate metus, at feugiat odio maximus vitae.'},
+    {title:'Curabitur sit amet risus nec tellus dictum pellentesque.', subTitle:'Morbi nec ipsum volutpat, sagittis libero ut, euismod ex. Nulla ex sapien, ultrices ac purus sit amet, finibus vestibulum mi. Nam ac sodales est. Fusce at mattis velit. Donec venenatis elit euismod, pellentesque ipsum vitae, laoreet lorem.'},
+    {title:'Sed tempor augue quis nisi tristique ultricies.', subTitle:'Curabitur mattis arcu sit amet enim scelerisque porta. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam quis cursus mauris, ac hendrerit sem. Praesent gravida leo ac dui rutrum eleifend vitae sed mauris. Duis faucibus, magna non interdum dapibus, augue dolor auctor velit, non suscipit leo lorem eu est.'}
+]
